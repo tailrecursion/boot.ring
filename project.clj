6163680a9ea89301f0de06dc693d88b27783a5e1
Micha Niskin <micha.niskin@gmail.com>
2014-03-16 11:00:13 -0400

    Update boot.core dependency

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
 (defproject tailrecursion/boot.ring "0.1.0-SNAPSHOT"
-  :description  "FIXME: write description"
-  :url          "http://example.com/FIXME"
+  :description  "Boot tasks to create ring server components."
+  :url          "https://github.com/tailrecursion/boot.ring"
   :license      {:name "Eclipse Public License"
                  :url  "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure     "1.5.1"]
-                 [tailrecursion/boot.core "2.2.0"]
-                 [ring                    "1.2.1"]]
+                 [ring                    "1.2.1"]
+                 [tailrecursion/boot.core "2.2.2-SNAPSHOT"]]
   :target-path  "target/%s"
   :profiles     {:uberjar {:aot :all}})
