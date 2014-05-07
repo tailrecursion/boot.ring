ed9565b86f9b710e22777b2a2fed74b37ef3abf9
Micha Niskin <micha.niskin@gmail.com>
2014-05-07 09:38:12 -0400

    0.1.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject tailrecursion/boot.ring "0.1.0-SNAPSHOT"
+(defproject tailrecursion/boot.ring "0.1.0"
   :description  "FIXME: write description"
   :url          "http://example.com/FIXME"
   :license      {:name "Eclipse Public License"
