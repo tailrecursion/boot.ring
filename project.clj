ff803a824d4b9533dfcb7411da3ca1b505eb3645
Micha Niskin <micha.niskin@gmail.com>
2014-06-05 15:06:05 -0400

    0.2.1 -- Update ring.middleware.cors dependency

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
-(defproject tailrecursion/boot.ring "0.2.0"
+(defproject tailrecursion/boot.ring "0.2.1"
   :description  "FIXME: write description"
   :url          "http://example.com/FIXME"
   :license      {:name "Eclipse Public License"
                  :url  "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[jumblerg/ring.middleware.cors "1.0.0"]
+  :dependencies [[jumblerg/ring.middleware.cors "1.0.1"]
                  [org.clojure/clojure           "1.5.1"]
                  [tailrecursion/boot.core       "2.2.0"]
                  [ring                          "1.2.1"]]
